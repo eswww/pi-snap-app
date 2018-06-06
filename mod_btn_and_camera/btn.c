b99a729ab4770baafe4aeee24da3bda04f977a68
SoYoung210 <ethdud1@gmail.com>
2018-06-06 13:29:42 +0900

    버튼 모듈 기본 셋팅 _ 맥에서 작성.

---
+++ mod_btn_and_camera/btn.c
@@ -0,0 +1,72 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/gpio.h>       // Required for the GPIO functions
+#include <linux/interrupt.h>  // Required for the IRQ code
+#include <linux/kobject.h>    // Using kobjects for the sysfs bindings
+
+#define gpioButton 115;
+#define  DEBOUNCE_TIME 200    ///< The default bounce time -- 200ms
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("test btn.ko and camera.py");
+MODULE_VERSION("0.1");
+
+static bool isRising = 1;                   ///< Rising edge is the default IRQ property
+static int  numberPresses = 0;            ///< For information, store the number of button presses for test.
+module_param(isRising, bool, S_IRUGO);      ///< Param desc. S_IRUGO can be read/not changed
+MODULE_PARM_DESC(isRising, " Rising edge = 1 (default), Falling edge = 0");  ///< parameter description
+
+//irq handle_button
+static irq_handler_t  ebbgpio_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs);
+
+static irq_handler_t ebbgpio_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs){
+
+   printk(KERN_INFO "EBB Button: The button state is currently: %d\n", gpio_get_value(gpioButton));
+   numberPresses++;                     // Global counter, will be outputted when the module is unloaded
+   return (irq_handler_t) IRQ_HANDLED;  // Announce that the IRQ has been handled correctly
+}
+
+static int __init ebbButton_init(void){
+   int result = 0;
+   unsigned long IRQflags = IRQF_TRIGGER_RISING;      // The default is a rising-edge interrupt
+
+   printk(KERN_INFO "EBB Button: Initializing the EBB Button\n");
+
+   // create the kobject sysfs entry at /sys/ebb -- probably not an ideal location!
+			                    // the bool argument prevents the direction from being changed
+   gpio_request(gpioButton, "sysfs");       // Set up the gpioButton
+   gpio_direction_input(gpioButton);        // Set the button GPIO to be an input
+   gpio_set_debounce(gpioButton, DEBOUNCE_TIME); // Debounce the button with a delay of 200ms
+   gpio_export(gpioButton, false);          // Causes gpio115 to appear in /sys/class/gpio
+			                    // the bool argument prevents the direction from being changed
+
+   // Perform a quick test to see that the button is working as expected on LKM load
+   printk(KERN_INFO "EBB Button: The button state is currently: %d\n", gpio_get_value(gpioButton));
+
+   /// GPIO numbers and IRQ numbers are not the same! This function performs the mapping for us
+   irqNumber = gpio_to_irq(gpioButton);
+   printk(KERN_INFO "EBB Button: The button is mapped to IRQ: %d\n", irqNumber);
+
+   if(!isRising){                           // If the kernel parameter isRising=0 is supplied
+      IRQflags = IRQF_TRIGGER_FALLING;      // Set the interrupt to be on the falling edge
+   }
+   // This next call requests an interrupt line
+   result = request_irq(irqNumber,             // The interrupt number requested
+                        (irq_handler_t) ebbgpio_irq_handler, // The pointer to the handler function below
+                        IRQflags,              // Use the custom kernel param to set interrupt type
+                        "ebb_button_handler",  // Used in /proc/interrupts to identify the owner
+                        NULL);                 // The *dev_id for shared interrupt lines, NULL is okay
+   return result;
+}
+static void __exit ebbButton_exit(void){
+   printk(KERN_INFO "EBB Button: The button was pressed %d times\n", numberPresses);
+
+   free_irq(irqNumber, NULL);               // Free the IRQ number, no *dev_id required in this case
+   gpio_unexport(gpioButton);               // Unexport the Button GPIO
+   gpio_free(gpioButton);                   // Free the Button GPIO
+   printk(KERN_INFO "EBB Button: Goodbye from the EBB Button LKM!\n");
+}
+
+module_init(ebbButton_init);
+module_exit(ebbButton_exit);
