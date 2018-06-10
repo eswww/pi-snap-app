61bff3e32ecd47c11707c82a501ea294d8a541d5
punkyoon <cryptography@konkuk.ac.kr>
2018-06-11 00:10:48 +0900

    added some comments and modified some code to make an application work safely

--- app/main.py
+++ app/main.py
@@ -11,33 +11,48 @@ from guizero import App, PushButton, Text, Picture
 from camera import Camera
 
 
+def queue_clear():
+    global q
+    
+    while not q.empty():
+        q.get()
+
 def cam_control():
     global cam, q
+
     cam.new_picture()
+    queue_clear()    # Ignore accumulated data"
+
     while True:
         data = q.get()
-        print('[consumer] ' + str(data))
+        print('[consumer] ' + data)
         if data != '1':
             continue
         cam.take_picture()
         break
+
     print('[consumer] done')
 
 def server_init():
-    print('server init')
+    print('[server] init')
     loop.create_task(server_task())
     loop.run_forever()
     loop.close()
 
 async def server_handler(conn):
-    global q
+    global SIZE, q
+
     while True:
         msg = await loop.sock_recv(conn, io.DEFAULT_BUFFER_SIZE)
-        print(msg, len(msg))
+
+        # Checking queue overflow
+        if(q.qsize() == SIZE - 1):
+            queue_clear()
         q.put(msg.decode('utf8'))
 
         if not msg:
             break
+
     conn.close()
 
 async def server_task():
@@ -47,25 +62,33 @@ async def server_task():
 
 
 if __name__ == '__main__':
-    host = '127.0.0.1'
-    port = 2324
-    q = Queue()
-
+    HOST = '127.0.0.1'
+    PORT = 2324
+    SIZE = 30    # Maximum queue size
+    
     cam = Camera()
+    q = Queue(maxsize=SIZE)
 
     # Setting server socket
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.setblocking(False)
-    server.bind((host, port))
+    server.bind((HOST, PORT))
     server.listen(10)
     loop = asyncio.get_event_loop()
 
+    # Running server process
     proc = Process(target=server_init)
     proc.start()
 
+    # Configuring GUI application
     app = App('Pi-Snap')
     txt = Text(app, 'Welcome to Pi-Snap!')
     new_pic_icon = PushButton(app, cam_control, text='New pic')
     take_pic_icon = PushButton(app, cam.take_picture, text='take pic')
+
+    # Running GUI application
     app.display()
+
+    # When the application is terminated, Server process is terminated
+    proc.terminate()
