8c26545535c4e8bd0f22320592b376371710fccc
punkyoon <cryptography@konkuk.ac.kr>
2018-06-10 23:08:14 +0900

    fix bug - #7

--- app/image_processing.py
+++ app/image_processing.py
@@ -9,47 +9,59 @@ from email.mime.application import MIMEApplication
 
 
 def insert_datetime(img_path):
+    # Open image
     img = Image.open(img_path).convert('RGBA')
 
-    ext_size = (img.size[0]+30, img.size[1]+120)
-    ext_img = Image.new('RGBA', ext_size, (0, 0, 0, 0))
-    ext_img.paste(img, (int((ext_size[0]-img.size[0])/2), 15))
+    # Add a border
+    border_size = (img.size[0] + 30, img.size[1] + 120)
+    border_img = Image.new('RGBA', border_size, (255, 255, 255, 255))
+    border_img.paste(img, (int((border_size[0] - img.size[0]) / 2), 15))
 
-    txt = Image.new('RGBA', ext_img.size, (0, 0, 0, 0))
-
+    # Add a text (datetime)
+    txt = Image.new('RGBA', border_img.size, (0, 0, 0, 0))
     fnt = ImageFont.truetype('./static/NanumGothicCoding.ttf', 30)
+    fnt_point = (15, border_img.size[1] - 65)
+
     tmp = ImageDraw.Draw(txt)
-
     today = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    tmp.text(
+        fnt_point,
+        today,
+        fill='black',
+        font=fnt,
+        align='center'
+    )
 
-    tmp.text((15, ext_img.size[1]-65), today, fill='white', font=fnt, align='center')
-
-    img_bin = Image.alpha_composite(ext_img, txt)
-    img_bin.save(img_path)
+    # Save image
+    img = Image.alpha_composite(border_img, txt)
+    img.save(img_path)
+    img.close()
 
 def insert_icon(img_path, light):
+    # Open image
     img = Image.open(img_path).convert('RGBA')
 
-    if (light):
+    # Day sun / Night moon
+    if light:
         icon = Image.open('./static/sun_icon.png')
     else:
         icon = Image.open('./static/moon_icon.png')
-    w_size = int(min(img.size[0], img.size[1]) * 0.25)
-    icon_percent = (w_size / float(icon.size[0]))
-    h_size = int((float(icon.size[1]) * float(icon_percent)))
 
-    s_image = icon.resize((int(w_size*0.8), int(h_size*0.8)))
-    mbox = img.getbbox()
-    sbox = s_image.getbbox()
+    # Find point
+    point = (
+        img.size[0] - icon.size[0] - 15,
+        img.size[1] - icon.size[1] - 7
+    )
 
-    box = (mbox[2] - sbox[2] - 10, mbox[3] - sbox[3]+30)
-    img.paste(s_image, box)
-
+    # Save image
+    img.paste(icon, point)
     img.save(img_path)
+    img.close()
 
 def send_email(img_path, from_email, to_email):
     img_name = os.path.basename(img_path)
 
+    # Email message object
     msg = EmailMessage()
     msg['Subject'] = 'Picture from Pi-Snap!'
     msg['From'] = from_email
@@ -64,13 +76,15 @@ def send_email(img_path, from_email, to_email):
         subtype='html'
     )
 
+    # Add an image to email
     with open(img_path, 'rb') as fp:
         img_bin = fp.read()
         
-        multi_part = MIMEApplication(img_bin, name=img_name)
-        multi_part.add_header('Content-ID', '<' + img_name + '>')
-        msg.attach(multi_part)
+        multipart = MIMEApplication(img_bin, name=img_name)
+        multipart.add_header('Content-ID', '<' + img_name + '>')
+        msg.attach(multipart)
 
+    # Send an email
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.ehlo()
         smtp.login(from_email, os.environ['GMAIL_PASS'])
